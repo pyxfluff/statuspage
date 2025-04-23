5e517b0a16d00fff8d41bc509b3faca66a9bccbd
Pyx <git@notpyx.me>
2025-04-23 18:02:36 -0400

    temp return errors

--- functions/api/status.js
+++ functions/api/status.js
@@ -19,7 +19,7 @@ export async function onRequestGet(context) {
         });
     } catch (e) {
         console.error(e);
-        return new Response("{}", {
+        return new Response(`{error: "${e}"}`, {
             status: 500,
             headers: {
                 "Content-Type": "application/json"
