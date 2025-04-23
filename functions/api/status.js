35be91b963bb84ac3a7bc993f5a2b61e913f3d73
Pyx <git@notpyx.me>
2025-04-23 18:30:37 -0400

    im gonna crash ouuutttt

--- functions/api/status.js
+++ functions/api/status.js
@@ -1,22 +1,20 @@
-// Copyright (c) 2024 iiPython
-
 export async function onRequestGet(context) {
-    console.log("hi")
     try {
         const urls = await context.env.statuspage_data.get("urls", { cacheTtl: 60 });
         const records = await context.env.statuspage_data.get("records", { cacheTtl: 60 });
 
-        let newServices = [];
-        Object.values(JSON.parse(urls)).forEach((service) => {
+        const newServices = {};
+
+        for (const service of JSON.parse(urls)) {
             try {
                 service.status = records[0].services[service.name]?.online ? "Online" : "Offline";
-                
-                newServices[service.name] = service
-            } catch (e) {
+            } catch {
                 service.status = "Migrating";
-                newServices[service.name] = service
             }
-        });
+            
+            newServices[service.name] = service;
+        }
+
 
         return new Response(JSON.stringify({
             urls: newServices,
